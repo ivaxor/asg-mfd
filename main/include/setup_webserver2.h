154852ab7bf1868c22f2e4d9c702b524ee18a20e
Illia Khovanskyi <saskiuhia@gmail.com>
2025-08-26 03:11:56 +0200

    Added 2nd version of captive portal

---
+++ main/include/setup_webserver2.h
@@ -0,0 +1,6 @@
+#ifndef SETUP_WEBSERVER2_H
+#define SETUP_WEBSERVER2_H
+
+void setup_webserver2(void);
+
+#endif // SETUP_WEBSERVER2_H
--- main/main.c
+++ main/main.c
@@ -3,14 +3,12 @@
 #include "freertos/task.h"
 #include "include/task_blink.h"
 #include "include/setup_webserver.h"
+#include "include/setup_webserver2.h"
 #include "include/setup_wifi.h"
-#include "wifi-captive-portal-esp-idf.h"
 
 void app_main(void)
 {
-    wifi_captive_portal_esp_idf();
-
-    //setup_webserver();
+    setup_webserver2();
 
     xTaskCreate(&task_blink, "task_blink", 2048, NULL, 5, NULL);
 }
